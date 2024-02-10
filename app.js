58d72c9a7c950f3012b0cd0828efcc8583503953
Sreeman Tummala <super.tummala@gmail.com>
2024-02-10 00:27:28 -0600

    more setup

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000; // Choose the port you want to run your server on
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
 // Define routes
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index.html')
 });
 
 // Start the server
